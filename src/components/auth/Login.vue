29140f9c8b8a4fedaef63a29f5617da28d86c527
Malov Victor <malvikus@gmail.com>
2019-05-15 13:19:27 +0300

    Add Login component

---
+++ src/components/auth/Login.vue
@@ -0,0 +1,87 @@
+<template>
+  <v-container>
+    <v-layout justify-center>
+      <v-flex shrink>
+        <div class="login">
+          <v-card>
+            <v-card-title>
+              <v-layout column>
+                <div class="field">
+                  <v-text-field label="E-mail" browser-autocomplete="email" v-model="email"/>
+                </div>
+
+                <div class="field">
+                  <v-text-field
+                    label="Password"
+                    :type="showPassword ? 'text' : 'password'"
+                    :append-icon="showPassword ? 'visibility' : 'visibility_off'"
+                    v-model="password"
+                    @click:append="showPassword = !showPassword"
+                  />
+                </div>
+
+                <v-btn color="success">Login</v-btn>
+                <div class="divider">
+                  <v-divider></v-divider>
+                </div>
+
+                <v-flex align-self-center mt-3>
+                  <span class="account-text">
+                    Don't have account?
+                    <router-link :to="{name: 'Signup'}">Signup</router-link>
+                  </span>
+                </v-flex>
+              </v-layout>
+            </v-card-title>
+          </v-card>
+        </div>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "Login",
+  data() {
+    return {
+      email: null,
+      password: null,
+      showPassword: false,
+      rules: {
+        required: value => !!value || "Required.",
+        min: v => v.length >= 6 || "Min 6 characters",
+        emailMatch: () => "The email and password you entered don't match"
+      }
+    };
+  }
+};
+</script>
+
+<style>
+.login {
+  min-width: 350px;
+  margin-top: 60px;
+}
+
+.login h2 {
+  font-size: 2.4em;
+}
+
+.login .field {
+  margin-bottom: 16px;
+  margin-left: 10px;
+  margin-right: 10px;
+}
+
+.login .divider {
+  margin-top: 6px;
+}
+
+.login .account-text {
+  font-size: 14px;
+  color: grey;
+}
+</style>
+
+
