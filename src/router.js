19c2ff6990b5914c8a932ef81f9f75133c386638
Malov Victor <malvikus@gmail.com>
2019-05-16 18:04:07 +0300

    Add nested routes

--- src/router.js
+++ src/router.js
@@ -1,8 +1,10 @@
 import Vue from "vue";
 import Router from "vue-router";
-import Home from "./views/Home.vue";
+import Bots from "@/views/Bots";
+import Frame from "@/views/Frame";
 import Signup from "@/components/auth/Signup";
 import Login from "@/components/auth/Login";
+import Settings from "@/components/settings/Settings";
 
 Vue.use(Router);
 
@@ -10,11 +12,6 @@ export default new Router({
   mode: "history",
   base: process.env.BASE_URL,
   routes: [
-    {
-      path: "/",
-      name: "home",
-      component: Home
-    },
     {
       path: "/signup",
       name: "Signup",
@@ -26,13 +23,30 @@ export default new Router({
       component: Login
     },
     {
-      path: "/about",
-      name: "about",
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () =>
-        import(/* webpackChunkName: "about" */ "./views/About.vue")
+      path: "",
+      name: "Frame",
+      component: Frame,
+      children: [
+        {
+          path: "/",
+          name: "Bots",
+          component: Bots
+        },
+        {
+          path: "/settings",
+          name: "Settings",
+          component: Settings
+        },
+        {
+          path: "/about",
+          name: "About",
+          // route level code-splitting
+          // this generates a separate chunk (about.[hash].js) for this route
+          // which is lazy-loaded when the route is visited.
+          component: () =>
+            import(/* webpackChunkName: "about" */ "./views/About.vue")
+        }
+      ]
     }
   ]
 });
