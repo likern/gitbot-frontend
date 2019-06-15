114f3e891dc2df033f05166ab3f0a276011f2aed
Malov Victor <malvikus@gmail.com>
2019-06-15 11:44:43 +0300

    Update icon name structure

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -3,13 +3,15 @@ import Vuetify from "vuetify/lib";
 import "vuetify/src/stylus/app.styl";
 import "@fortawesome/fontawesome-pro/css/all.css";
 
+const ICONS = {
+  close: "fal fa-times",
+  "box-close": "fas fa-times-square",
+  "box-minus": "fas fa-minus-square",
+  "box-empty": "far fa-square",
+  "box-selected": "fas fa-check-square"
+};
+
 Vue.use(Vuetify, {
   iconfont: "fa",
-  icons: {
-    close: "fal fa-times",
-    "box-close": "fas fa-times-square",
-    "box-minus": "fas fa-minus-square",
-    "box-empty": "far fa-square",
-    "box-selected": "fas fa-check-square"
-  }
+  icons: ICONS
 });
